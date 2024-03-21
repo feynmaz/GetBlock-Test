24d437f1b6d86c8077cb391901266b9b001dd4b0
Nikolay Mazein <mazein.nikolay@wb.ru>
2024-03-22 03:58:21 +0500

    implement basic request/read

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 
 	"github.com/feynmaz/GetBlock-Test/config"
 	"github.com/feynmaz/GetBlock-Test/tools/logger"
@@ -21,6 +24,36 @@ func run() error {
 		return fmt.Errorf("failed to init logger: %w", err)
 	}
 
-	
+	url := fmt.Sprintf("https://go.getblock.io/%s", cfg.AccessToken)
+	bodyJson := `{
+		"id": "blockNumber",
+		"jsonrpc": "2.0",
+		"method": "eth_getBlockByNumber",
+		"params": [
+			"latest",
+			false
+		]
+	}`
+	requestBody := bytes.NewBuffer([]byte(bodyJson))
+	request, err := http.NewRequest("POST", url, requestBody)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+	request.Header.Add("Content-Type", "application/json")
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return fmt.Errorf("failed to do request: %w", err)
+	}
+	defer response.Body.Close()
+
+	responseBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	// Print the response body
+	fmt.Println("Response:", string(responseBody))
+
 	return nil
 }
