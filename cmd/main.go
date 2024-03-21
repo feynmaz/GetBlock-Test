5e6413d564669cc8e3506dc8cd55935e6bd8cc7b
Nikolay Mazein <mazein.nikolay@wb.ru>
2024-03-22 03:33:21 +0500

    add configs

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+
+	"github.com/feynmaz/GetBlock-Test/config"
+	"github.com/feynmaz/GetBlock-Test/tools/logger"
+)
 
 func main() {
 	if err := run(); err != nil {
@@ -9,5 +15,12 @@ func main() {
 }
 
 func run() error {
+	cfg := config.GetDefault()
+
+	if err := logger.InitLogrus(cfg.LogLevel, cfg.LogJson); err != nil {
+		return fmt.Errorf("failed to init logger: %w", err)
+	}
+
+	
 	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/feynmaz/GetBlock-Test
 
 go 1.21.8
+
+require (
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
